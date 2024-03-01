0c8ba5060a1de339b87f0653c8a121fa2f491bc8
KhaledElmorsy <khaled.mg.a@gmail.com>
2024-03-01 14:05:31 +0200

    fix(services): Correct the user service export.
    It was exporting auth. as user 🤦‍♂️

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,4 @@
 export { post } from './post';
 export * as authorization from './authorization';
-export * as user from './authorization';
+export * as user from './user';
 export { comment } from './comment';
